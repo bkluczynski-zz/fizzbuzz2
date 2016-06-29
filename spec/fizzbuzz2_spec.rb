f62ab5a5333894c995e750b1fd5cfcbaa3d10b55
Bart <kluczynskib@gmail.com>
2016-06-29 13:18:07 +0100

    multiple by 5 fails

--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -5,3 +5,9 @@ describe "fizzbuzz2" do
     expect(fizzbuzz2(3)).to eq "fizz"
   end
 end
+
+describe "fizzbuzz2" do
+  it "Checks if number is dividable by 5" do
+    expect(fizzbuzz2(5)).to eq "buzz"
+  end
+end
