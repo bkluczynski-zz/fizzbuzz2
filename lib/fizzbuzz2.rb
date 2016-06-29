5cd9e553bd039f4f8cd0f99a15ef7e2dafcd3918
Bart <kluczynskib@gmail.com>
2016-06-29 13:16:21 +0100

    multiple by 3 passes

---
+++ lib/fizzbuzz2.rb
@@ -0,0 +1,10 @@
+def fizzbuzz2(number)
+
+if number%3 == 0
+  return "fizz"
+end
+
+
+
+
+end
--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -1,7 +1,7 @@
 require 'fizzbuzz2'
 
-describe "fizzbuzz" do
+describe "fizzbuzz2" do
   it "Checks if number is dividable by 3" do
-    expect(fizzbuzz(3)).to eq "fizz"
+    expect(fizzbuzz2(3)).to eq "fizz"
   end
 end
